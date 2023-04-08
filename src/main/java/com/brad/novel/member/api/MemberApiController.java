96dfe15675f468ee058dc058fa550eda445651da
yoodongan <dbehddks519@naver.com>
2023-04-08 15:13:22 +0900

    회원 api 컨트롤러 추가

--- src/main/java/com/brad/novel/member/api/MemberApiController.java
+++ src/main/java/com/brad/novel/member/api/MemberApiController.java
@@ -3,13 +3,14 @@ package com.brad.novel.member.api;
 import com.brad.novel.member.dto.JoinResponse;
 import com.brad.novel.member.dto.MemberJoinRequestDto;
 import com.brad.novel.member.service.MemberService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
