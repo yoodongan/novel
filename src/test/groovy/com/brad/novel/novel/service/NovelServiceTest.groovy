d22db8e7196b2164b04649118cd95f1776338cc0
yoodongan <dbehddks519@naver.com>
2023-04-11 15:31:58 +0900

    소설 저장 spock 테스트 추가

---
+++ src/test/groovy/com/brad/novel/novel/service/NovelServiceTest.groovy
@@ -0,0 +1,54 @@
+package com.brad.novel.novel.service
+
+import com.brad.novel.member.dto.MemberJoinRequestDto
+import com.brad.novel.member.service.MemberService
+import com.brad.novel.novel.dto.NovelRequestDto
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.transaction.annotation.Transactional
+import spock.lang.Specification
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
+class NovelServiceTest extends Specification {
+    @Autowired
+    NovelService novelService;
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+
+    def setup() {
+        setup:
+        def name = "김철수"
+        def password = "1234"
+        def memberJoinRequestDto = new MemberJoinRequestDto()
+        memberJoinRequestDto.name = name
+        memberJoinRequestDto.password = password
+
+        def joinName = memberService.join(memberJoinRequestDto)
+    }
+
+    def "소설 저장"() {
+        setup:
+        def novelRequestDto = new NovelRequestDto()
+        novelRequestDto.subject = "소설A"
+        novelRequestDto.genre = "스릴러"
+        novelRequestDto.description = "이것은 스릴러 장르로, 마지막에 반전이 숨겨진 흥미진진한 소설입니다."
+        novelRequestDto.authorName = "작가A"
+        novelRequestDto.imagePath = "sample-img-URL"
+
+        when:
+        def novelId = novelService.save(novelRequestDto)
+        def findNovel = novelService.findById(novelId)
+
+        then:
+        novelRequestDto.subject == findNovel.subject
+
+    }
+
+}
