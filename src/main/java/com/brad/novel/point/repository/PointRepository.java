3df745b6fb978a236dc42f992003e21045007667
yoodongan <dbehddks519@naver.com>
2023-04-16 18:11:42 +0900

    포인트 충전 중복 요청 처리 -포인트 충전 동일한 요청 n 번 발생해도 1번만 처리되도록 구현

---
+++ src/main/java/com/brad/novel/point/repository/PointRepository.java
@@ -0,0 +1,16 @@
+package com.brad.novel.point.repository;
+
+import com.brad.novel.point.entity.Point;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.persistence.LockModeType;
+import java.util.Optional;
+
+public interface PointRepository extends JpaRepository<Point, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select p from Point p where p.preventDupId = :preventDupId")
+    public Optional<Point> findByIdLock(@Param("preventDupId") String preventDupId);
+}
