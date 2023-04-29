bbafa80cc50fedc84caaaf0de883d4281a265ae1
yoodongan <dbehddks519@naver.com>
2023-04-29 19:32:18 +0900

    Preference 컨트롤러, 서비스 계층 책임 분리

--- src/main/java/com/brad/novel/preference/api/PreferenceApiController.java
+++ src/main/java/com/brad/novel/preference/api/PreferenceApiController.java
@@ -1,9 +1,17 @@
 package com.brad.novel.preference.api;
 
+import com.brad.novel.common.error.ResponseCode;
 import com.brad.novel.common.response.DataResponse;
+import com.brad.novel.preference.dto.BestPreferenceDto;
+import com.brad.novel.preference.dto.ResultPreferenceDto;
+import com.brad.novel.preference.entity.Preference;
 import com.brad.novel.preference.service.PreferenceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,12 +21,21 @@ public class PreferenceApiController {
     /* 유저 별 선호한 작품 목록 조회 */
     @GetMapping("/preference/{memberId}")
     public DataResponse getPreference(@PathVariable Long memberId, @RequestParam Integer likeNumber) {
-        return preferenceService.getPreferenceNovel(memberId, likeNumber);
+        List<Preference> preferences = preferenceService.getPreferenceNovel(memberId, likeNumber);
+        List<ResultPreferenceDto> results = preferences.stream()
+                .map(p -> ResultPreferenceDto.of(p.getNovel(), p.getRecentCh()))
+                .collect(Collectors.toList());
+        return new DataResponse(ResponseCode.SUCCESS_202, results);
     }
 
     /* 1시간 간격으로 갱신, 베스트 선호 작품 조회 */
     @GetMapping("/preference/preferPerHour")
     public DataResponse getBestPreferencePerHour() {
-        return preferenceService.getBestPreferencePerHour();
+        List<Object[]> bestPreferencesPerHour = preferenceService.getBestPreferencePerHour();
+        List<BestPreferenceDto> bestPreferenceDtos = new ArrayList<>();
+        for (Object[] objects : bestPreferencesPerHour) {
+            bestPreferenceDtos.add(new BestPreferenceDto((Long) objects[0], (Long) objects[1]));
+        }
+        return new DataResponse(ResponseCode.SUCCESS_202, bestPreferenceDtos);
     }
 }
--- src/main/java/com/brad/novel/preference/service/PreferenceService.java
+++ src/main/java/com/brad/novel/preference/service/PreferenceService.java
@@ -1,21 +1,16 @@
 package com.brad.novel.preference.service;
 
-import com.brad.novel.common.response.DataResponse;
 import com.brad.novel.global.redis.RedisCacheKey;
-import com.brad.novel.preference.dto.BestPreferenceDto;
-import com.brad.novel.preference.dto.ResultPreferenceDto;
 import com.brad.novel.preference.entity.Preference;
 import com.brad.novel.preference.repository.PreferenceRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -23,26 +18,17 @@ import java.util.stream.Collectors;
 @CacheConfig(cacheNames = {"preference"})
 public class PreferenceService {
     private final PreferenceRepository preferenceRepository;
-    private final RedisTemplate<String, Object> redisTemplate;
 
-    public DataResponse getPreferenceNovel(Long memberId, Integer likeNumber) {
+    @Transactional(readOnly = true)
+    public List<Preference> getPreferenceNovel(Long memberId, Integer likeNumber) {
         List<Preference> preferences = preferenceRepository.findByMemberIdAndLikeNumberOverTwo(memberId, likeNumber);
-        List<ResultPreferenceDto> results = preferences.stream()
-                .map(p -> ResultPreferenceDto.of(p.getNovel(), p.getRecentCh()))
-                .collect(Collectors.toList());
-        return DataResponse.success(results);
+        return preferences;
     }
 
+    @Transactional(readOnly = true)
     @Cacheable(value = RedisCacheKey.PREFERENCE_LIST)
-    public DataResponse getBestPreferencePerHour() {
-        List<Object[]> best = preferenceRepository.findByBestReferencePerHour();
-        List<BestPreferenceDto> bestPreferenceDtos = new ArrayList<>();
-        for (Object[] objects : best) {
-            bestPreferenceDtos.add(new BestPreferenceDto((Long) objects[0], (Long) objects[1]));
-        }
-        return DataResponse.success(bestPreferenceDtos);
+    public List<Object[]> getBestPreferencePerHour() {
+         return preferenceRepository.findByBestReferencePerHour();
     }
-
-
 
 }
