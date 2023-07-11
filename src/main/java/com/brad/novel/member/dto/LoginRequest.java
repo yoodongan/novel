077b877eb2a81a46848fe55272e539152d261ecb
yoodongan <dbehddks519@naver.com>
2023-07-11 15:48:42 +0900

    로그인 시 전달할 LoginRequest Dto 추가

---
+++ src/main/java/com/brad/novel/member/dto/LoginRequest.java
@@ -0,0 +1,13 @@
+package com.brad.novel.member.dto;
+
+import lombok.Data;
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class LoginRequest {
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String password;
+}
